b5c3513c55b8ffb70b1ff656470913f0a979371b
chakri1293 <149371921+chakri1293@users.noreply.github.com>
2023-12-19 22:31:21 +0530

    Initial commit

---
+++ strmlit_html.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    # Load your HTML content
+    with open("index.html", "r") as f:
+        html_content = f.read()
+
+    # Display the HTML content
+    st.write(html_content, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
