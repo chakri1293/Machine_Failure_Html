4f704d2c0d8f04bbda9ca46b27f0e7355b659a53
chakri1293 <149371921+chakri1293@users.noreply.github.com>
2023-12-19 22:27:45 +0530

    Update strmlit_html.py

--- base_html/strmlit_html.py
+++ base_html/strmlit_html.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     # Load your HTML content
-    with open("index.html", "r") as f:
+    with open("base_html/index.html", "r") as f:
         html_content = f.read()
 
     # Display the HTML content
