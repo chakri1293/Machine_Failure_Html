273d382797b406251e62996b652834c52fb44bed
chakri1293 <149371921+chakri1293@users.noreply.github.com>
2023-12-19 22:54:01 +0530

    Update strmlit_html.py

--- strmlit_html.py
+++ strmlit_html.py
@@ -1,12 +1,20 @@
 import streamlit as st
+import os
 
 def main():
+    # Print current working directory
+    st.write("Current Working Directory:", os.getcwd())
+
+    # List contents of the "assets" folder
+    assets_folder = "assets"
+    st.write("Contents of 'assets' folder:", os.listdir(assets_folder))
+
     # Load your HTML content
     with open("index.html", "r") as f:
         html_content = f.read()
 
     # Display the HTML content
-    st.markdown(html_content, unsafe_allow_html=True)
-    
+    st.write(html_content, unsafe_allow_html=True)
+
 if __name__ == "__main__":
     main()
